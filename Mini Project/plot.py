ae078811cfe9c54ad80f3dff9aedbb485dfec5a7
Bishal Joshi <bishaljoshi08@gmail.com>
2022-05-18 23:20:44 -0400

    Add images : Mini Project

--- Mini Project/plot.py
+++ Mini Project/plot.py
@@ -26,15 +26,14 @@ global n,k
 while(True):
     n = int(input("n: "))
     k = int(input("k: "))
-    print(2*k+1<n)
-    if k>1 and n>3 and 2*k +1 <= n:
+    if k>=1 and n>=3 and 2*k +1 <= n:
         break
     else:
-        print('invalid format')
+        print('Invalid Format')
 
 glutInit()
 glutInitDisplayMode(GLUT_RGB)
-glutCreateWindow("Drawing")
+glutCreateWindow(f"Polygram {n}/{k}")
 glutInitWindowSize(500, 500)
 glutInitWindowPosition(50, 50)
 glutDisplayFunc(plot_points)
